2646c12f2d33c01b9b9981b103435c652e3aa7f7
Dmitry Konishchev <konishchev@gmail.com>
2009-07-09 14:30:05 +0000

    git-svn-id: https://flush.svn.sourceforge.net/svnroot/flush/trunk@73 21f5d154-ea94-4097-a85b-94d12d4401c9

--- src/mlib/misc.cpp
+++ src/mlib/misc.cpp
@@ -645,7 +645,7 @@ void unix_epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
 
 int unix_epoll_create(void)
 {
-	int fd = epoll_create1(0);
+	int fd = epoll_create(0);
 	if(fd < 0)
 		M_THROW_SYS(errno);
 	else
